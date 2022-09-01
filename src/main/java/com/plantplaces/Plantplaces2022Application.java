8493e57e3ec202e35d8fd493d1a8ea56948c564b
Administrator <Administrator@DESKTOP-099UQRG>
2022-09-01 14:20:04 -0400

    Initial Commit

---
+++ src/main/java/com/plantplaces/Plantplaces2022Application.java
@@ -0,0 +1,13 @@
+package com.plantplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plantplaces2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plantplaces2022Application.class, args);
+	}
+
+}
